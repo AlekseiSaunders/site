9fda5d42b42f6275ec5523bfbc9d944c01baf7cb
Aleksei Saunders <alekseisaunders@gmail.com>
2022-08-17 11:15:28 -0600

    add body parser to handle form req.body, add partials, add weatherMiddleware

--- meadowlark.js
+++ meadowlark.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { engine } = require('express-handlebars');
 const handlers = require('./lib/handlers');
+const weatherMiddleware = require('./lib/middleware/weather');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3300;
@@ -11,6 +13,7 @@ app.engine(
   engine({
     defaultLayout: 'main',
     extname: '.hbs',
+    partialsDir: 'views/partials',
     helpers: {
       section: function (name, options) {
         if (!this._sections) this._sections = {};
@@ -25,8 +28,11 @@ app.set('view engine', 'hbs');
 // configure public to serve static files
 app.use(express.static(__dirname + '/public'));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // create root route
 app.get('/', handlers.home);
+app.get('/section-test', handlers.sectionTest);
 
 // create About route
 app.get('/about', handlers.about);
@@ -36,6 +42,8 @@ app.use(handlers.notFound);
 
 // custom 500 page
 app.use(handlers.serverError);
+
+app.use(weatherMiddleware);
 
 // allow app to be required as module for testing with puppeteer
 if (require.main === module) {
